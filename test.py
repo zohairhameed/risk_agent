75b0fccfbabff49f3c6e1a44c650b2c542e2978d
Zohair Hameed <zohair.hameed@outlook.com>
2025-08-23 23:19:56 +0400

    revisions

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3, numpy as np
-
-# 1) Read delivery days
-conn = sqlite3.connect('risk.db')
-days = [d[0] for d in conn.execute("SELECT delivery_days FROM suppliers").fetchall()]
-conn.close()
-
-worst = np.random.choice(days, 1000)
-print(worst)
